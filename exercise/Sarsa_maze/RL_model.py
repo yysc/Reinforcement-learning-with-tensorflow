03c5d8f4407bb81a23fc7a5a1c06358a30fe2eb2
yy <yuanyy@cn.ibm.com>
2017-08-10 14:02:45 +0800

    add sarsa table

--- exercise/Sarsa_maze/RL_model.py
+++ exercise/Sarsa_maze/RL_model.py
@@ -2,11 +2,65 @@ import numpy as np
 import pandas as pd
 
 
-class SarsaLambaTable(object):
+class RL(object):
 
     def __init__(self, action_space, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
         self.actions = action_space
+        self.learning_rate = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions)
 
-    def choose_action(state, q_table):
-        self.check_state_exists(state)
-        if np.random.uniform()>self.e
+    def choose_action(self, observation):
+        self.check_state_exists(observation)
+        if np.random.uniform() < self.epsilon:
+            state_action = self.q_table.ix[observation, :]
+            state_action = state_action.reindex(
+                np.random.permutation(state_action.index))
+            action = state_action.argmax()
+        else:
+            action = np.random.choice(self.actions)
+
+        return action
+
+    def check_state_exists(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state
+                )
+            )
+
+
+class QLearningTable(RL):
+
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        super(QLearningTable, self).__init__(
+            actions, learning_rate, reward_decay, e_greedy)
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exists(s_)
+        q_predict = self.q_table.ix[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+        else:
+            q_target = r
+        self.q_table.ix[s, a] += self.learning_rate * (q_target - q_predict)
+
+
+class SarsaTable(RL):
+
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        super(SarsaTable, self).__init__(
+            actions, learning_rate, reward_decay, e_greedy)
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exists(s_)
+        q_predict = self.q_table.ix[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.ix[s_, a_]
+        else:
+            q_target = r
+        self.q_table.ix[s, a] += self.learning_rate * (q_target - q_predict)
